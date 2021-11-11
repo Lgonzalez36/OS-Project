366d56c98b6d15d8a0c3f057c7cc20e73d22367b
Zarrukh Bazarov <zarrukh.bazarov@gmail.com>
2021-11-11 11:42:25 -0600

    Documentation 1.1
    Adding documentation/comments to SearchDubFiles class

--- SearchDubFiles.java
+++ SearchDubFiles.java
@@ -9,15 +9,39 @@ import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-
+/***************************************************************************************
+ * SearchDubFiles Class is used to search for Duplicate files, show the user
+ * the results of the search and delete them if found. !!!!!! //Is It deleting?
+ *
+ * @author Luis Gonzales, Zarrukh Bazarov, Austin Krause
+ * @version November, 2021
+ *
+ ***************************************************************************************/
 public class SearchDubFiles{
+
+    /** Declaring an instance of SeparateChainingHashST, used in search method */
     private SeparateChainingHashST<String, String> hashTableST;
+
+    /** dir stands for Directory name, which the user inputs */
     private String dir;
 
+
+    /**********************************************************************
+     * Constructor helps to create the SearchDubFiles object with the
+     * directory specified by the parameter.
+     *
+     * @param dir is the name of the directory which the user inputs
+     **********************************************************************/
     SearchDubFiles(String dir){
         this.dir = dir;
     }
 
+    /***********************************************************************************
+     * Search method is used to search for directory for Duplicate files.
+     *
+     * @throws NoSuchAlgorithmException when requested algorithm is not available
+     * @throws IOException if operation fails to execute
+     ***********************************************************************************/
     public void search() throws NoSuchAlgorithmException, IOException{
         File folder = new File(this.dir);          // in windows is C:\\Desktop\\...
         File[] listOfFiles = folder.listFiles();    // in linux is "/home/lagonzalez/Documents/OS-Project"/...
@@ -44,7 +68,9 @@ public class SearchDubFiles{
                 i++;
             }
         }
-        System.out.println("____________________________________________________________\n");
+        System.out.println("____________________________________________________________\n"); // page break
+
+        //
         hashTableST.printST();
     }
 }
