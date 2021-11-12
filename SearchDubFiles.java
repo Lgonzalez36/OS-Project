45380a8a4e3342ca727321e247a300fbdfb29d7b
Austin Krause <62400462+austinlefty@users.noreply.github.com>
2021-11-11 23:45:19 -0600

    Update SearchDubFiles.java
    Updated hashing function (Now works with all directories with accessible files)

--- SearchDubFiles.java
+++ SearchDubFiles.java
@@ -1,11 +1,8 @@
-/**
- * @author
- * @since November 11, 2021
- */
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -42,35 +39,39 @@ public class SearchDubFiles{
      * @throws NoSuchAlgorithmException when requested algorithm is not available
      * @throws IOException if operation fails to execute
      ***********************************************************************************/
-    public void search() throws NoSuchAlgorithmException, IOException{
-        File folder = new File(this.dir);          // in windows is C:\\Desktop\\...
-        File[] listOfFiles = folder.listFiles();    // in linux is "/home/lagonzalez/Documents/OS-Project"/...
-        hashTableST = new SeparateChainingHashST<>((listOfFiles.length));
-        System.out.println("Total files:\t" + listOfFiles.length + "\n");
-        System.out.println("#" + "\t" + "HashCode" + "\t\t\t\t" + "File Name\n");
-        int i=0;
-        MessageDigest shaDigest = MessageDigest.getInstance("MD5");
-        for (File file : listOfFiles){
-            if (file.isFile()) {
-                FileInputStream fis = new FileInputStream(file.getName());
-                byte[] byteArray = new byte[1024];
-                int bytesCount = 0; 
-                while ((bytesCount = fis.read(byteArray)) != -1) {
-                    shaDigest.update(byteArray, 0, bytesCount);
-                }
+    public void search() throws NoSuchAlgorithmException, IOException {
+        //File Variables
+        File file = new File(this.dir);
+        File[] files = file.listFiles();
+        String fileName;
+
+        hashTableST = new SeparateChainingHashST<>((files.length));
+
+        //Hash Variables
+        byte[] bytes, digestedBytes;
+        MessageDigest md;
+
+        //Traverses through every file in the files array
+        for (int i = 0; i < files.length; i++){
+            //Checks if index is a file
+            if(files[i].isFile()) {
+                fileName = files[i].getName(); //Temporary file name variable
+                bytes = Files.readAllBytes(files[i].toPath()); //Reads all bytes from file
+                md = MessageDigest.getInstance("MD5"); //MD5 is a cryptographic hash function, unique enough for this program
+                md.update(bytes); //Updates the digest
+                digestedBytes = md.digest(); //Completes hash computation
+
+                //StringBuilder is faster and uses less memory than String
                 StringBuilder sb = new StringBuilder();
-                byte[] bytes = shaDigest.digest();
-                for(int j=0; j< bytes.length ;j++){
-                    sb.append(Integer.toString((bytes[j] & 0xff) + 0x100, 16).substring(1));
-                }
-                hashTableST.put(sb.toString(), file.getName());
-                System.out.println((i+1) + "\t" + sb.toString() + "\t" + file.getName());
-                i++;
+                //Appends hash to string
+                for (int j = 0; j < digestedBytes.length; j++)
+                    sb.append(Integer.toString((digestedBytes[j] & 0xff) + 0x100, 16).substring(1));
+
+                hashTableST.put(sb.toString(), fileName); //Puts file in list to check for duplicates
+                System.out.println((i) + ":\t" + sb + "\t" + fileName); //Prints hash and file
             }
         }
-        System.out.println("____________________________________________________________\n"); // page break
-
-        //
+        
         hashTableST.printST();
     }
 }
